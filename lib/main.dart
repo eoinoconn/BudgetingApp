521f020ca77cc094c4e921ff6be1655e2bb715a5
eoinoconn <eoinoconn@gmail.com>
2023-05-18 18:46:57 +0700

    basic form implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,301 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: CardFormWidget(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class CardFormWidget extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TripSpend',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  _CardFormWidgetState createState() => _CardFormWidgetState();
+}
+
+class _CardFormWidgetState extends State<CardFormWidget>
+    with SingleTickerProviderStateMixin {
+  String description = '';
+  double cost = 0.0;
+  String currency = 'EUR';
+  String category = 'Food & Drink';
+  String location = 'Thailand';
+  DateTime date = DateTime.now();
+  String notes = '';
+
+  bool isFormVisible = false;
+
+  late AnimationController _animationController;
+  late Animation<Offset> _offsetAnimation;
+
+  List<String> currencyOptions = ['EUR', 'USD', 'THB'];
+  List<String> categoryOptions = ['Food & Drink', 'Transport'];
+  List<String> locationOptions = ['Thailand', 'Cambodia'];
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: Duration(milliseconds: 300),
+      vsync: this,
     );
+    _offsetAnimation = Tween<Offset>(
+      begin: Offset(0, 1),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeOut,
+    ));
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _showForm() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      isFormVisible = true;
+    });
+    _animationController.forward();
+  }
+
+  void _hideForm() {
+    _animationController.reverse().then((value) {
+      setState(() {
+        isFormVisible = false;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Expense Tracker'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      floatingActionButton: FloatingActionButton(
+        onPressed: isFormVisible ? null : _showForm,
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Container(
+          height: 48.0,
+          color: Colors.blue,
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Visibility(
+              visible: isFormVisible,
+              child: SlideTransition(
+                position: _offsetAnimation,
+                child: Card(
+                  elevation: 4.0,
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.close),
+                              onPressed: _hideForm,
+                            ),
+                          ],
+                        ),
+                        Text(
+                          'Add Expense',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 16.0),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Description',
+                          ),
+                          onChanged: (value) {
+                            setState(() {
+                              description = value;
+                            });
+                          },
+                        ),
+                        SizedBox(height: 16.0),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Cost',
+                          ),
+                          keyboardType:
+                              TextInputType.numberWithOptions(decimal: true),
+                          onChanged: (value) {
+                            setState(() {
+                              cost = double.tryParse(value) ?? 0.0;
+                            });
+                          },
+                        ),
+                        SizedBox(height: 16.0),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: DropdownButtonFormField<String>(
+                                decoration: InputDecoration(
+                                  labelText: 'Currency',
+                                ),
+                                value: currency,
+                                items: currencyOptions.map((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                                onChanged: (value) {
+                                  setState(() {
+                                    currency = value!;
+                                  });
+                                },
+                              ),
+                            ),
+                            SizedBox(width: 16.0),
+                            Expanded(
+                              child: DropdownButtonFormField<String>(
+                                decoration: InputDecoration(
+                                  labelText: 'Category',
+                                ),
+                                value: category,
+                                items: categoryOptions.map((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                                onChanged: (value) {
+                                  setState(() {
+                                    category = value!;
+                                  });
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16.0),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: DropdownButtonFormField<String>(
+                                decoration: InputDecoration(
+                                  labelText: 'Location',
+                                ),
+                                value: location,
+                                items: locationOptions.map((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                                onChanged: (value) {
+                                  setState(() {
+                                    location = value!;
+                                  });
+                                },
+                              ),
+                            ),
+                            SizedBox(width: 16.0),
+                            Expanded(
+                              child: GestureDetector(
+                                onTap: () async {
+                                  final DateTime? selectedDate =
+                                      await showDatePicker(
+                                    context: context,
+                                    initialDate: date,
+                                    firstDate: DateTime(2000),
+                                    lastDate: DateTime(2100),
+                                  );
+                                  if (selectedDate != null) {
+                                    setState(() {
+                                      date = selectedDate;
+                                    });
+                                  }
+                                },
+                                child: InputDecorator(
+                                  decoration: InputDecoration(
+                                    labelText: 'Date',
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      Icon(Icons.calendar_today),
+                                      SizedBox(width: 8.0),
+                                      Text(date.toString()),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16.0),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Notes',
+                          ),
+                          maxLines: 3,
+                          onChanged: (value) {
+                            setState(() {
+                              notes = value;
+                            });
+                          },
+                        ),
+                        SizedBox(height: 16.0),
+                        ElevatedButton(
+                          child: Text('Submit'),
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('Card Information'),
+                                  content: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Text('Description: $description'),
+                                      Text('Cost: $cost'),
+                                      Text('Currency: $currency'),
+                                      Text('Category: $category'),
+                                      Text('Location: $location'),
+                                      Text('Date: ${date.toLocal()}'),
+                                      Text('Notes: $notes'),
+                                    ],
+                                  ),
+                                  actions: [
+                                    ElevatedButton(
+                                      child: Text('OK'),
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
